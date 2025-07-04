6568ee7b918c1ed984cf58c3562488bd09716e79
Youssefr1 <youssframa09@gmail.com>
2025-07-04 18:20:12 +0300

    add add notes state

--- lib/cubits/add_note_cubit/add_note__state.dart
+++ lib/cubits/add_note_cubit/add_note__state.dart
@@ -4,3 +4,11 @@ part of 'add_note__cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+final class AddNoteLoading extends AddNoteState {}
+final class AddNoteSuccess extends AddNoteState {}
+final class AddNoteFailure extends AddNoteState {
+  final String errmassage;
+
+  AddNoteFailure(this.errmassage);
+}
+
