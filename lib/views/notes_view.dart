ff2d41831936c9a12836281d1d40eda99f233694
Youssefr1 <youssframa09@gmail.com>
2025-07-23 02:31:04 +0300

    add fetchAllNotes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/widjets/add_note_buttom.dart';
 import 'package:notesapp/widjets/notes_view_body.dart';
+
+import '../cubits/notes_cubit/notes_cubit.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.greenAccent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.greenAccent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24)
+              ),
+              context: context,
+              builder: (context) {
+                return AddNoteButtom();
+              },
+            );
+          },
+          child: Icon(Icons.add),
         ),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape:RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24)
-            ) ,
-            context: context,
-            builder: (context) {
-              return AddNoteButtom();
-            },
-          );
-        },
-        child: Icon(Icons.add),
+        body: NotesViewBody(),
       ),
-      body: NotesViewBody(),
     );
   }
 }
